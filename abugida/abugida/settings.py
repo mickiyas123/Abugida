27caf4064d7f2673d42197fce1848303ebb6e756
abdisag1 <myabdisa@gmailcom>
2022-05-10 21:34:55 +0300

    done models for user and discutions,created app discutions and user

--- abugida/abugida/settings.py
+++ abugida/abugida/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'discussions.apps.DiscussionsConfig',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
