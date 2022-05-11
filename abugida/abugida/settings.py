f078bcc08a5944f40a7af3177f2247e73d122c71
mickiy123 <mikikinfe345@gmail.com>
2022-05-11 14:18:25 +0300

    Register templates

--- abugida/abugida/settings.py
+++ abugida/abugida/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'abugida.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'templates' ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
