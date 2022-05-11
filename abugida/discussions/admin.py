f77273e66fe9f3c94b834181a6ccdc2a7be5a0ac
mickiy123 <mikikinfe345@gmail.com>
2022-05-11 16:00:00 +0300

    discussions/

--- abugida/discussions/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Topic, Room, Question, Answer 
-
-# Register your models here.
-
-admin.site.register(Topic)
-admin.site.register(Room)
-admin.site.register(Question)
-admin.site.register(Answer)
