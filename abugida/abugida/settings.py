d3bd1a582f9a6833ca252dde89090bdb8b0ce4fe
Mickiyas Kinfemichael <57702109+mickiyas123@users.noreply.github.com>
2022-05-11 16:12:26 +0300

    Update settings.py

--- abugida/abugida/settings.py
+++ abugida/abugida/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'abugida.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / 'templates' ],
+        'DIRS': [ ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
