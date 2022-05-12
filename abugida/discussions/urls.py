543edef8c41c42eddca6c0a0b0e51544520a6905
abdisag1 <myabdisa@gmail.com>
2022-05-12 19:38:44 +0300

    fixed css integration

--- abugida/discussions/urls.py
+++ abugida/discussions/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello', views.hello, name="hello"),
+    path('', views.discussions, name="discussions"),
     
     
 ]
