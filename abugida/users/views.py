3d5c2485a607ab9ca2696c24c56d4c7141053422
mickiy123 <mikikinfe345@gmail.com>
2022-05-13 14:18:45 +0300

    Completed login functionality

--- abugida/users/views.py
+++ abugida/users/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
 from .models import Profile
 
 # Create your views here.
 
+
+def loginPage(request):
+    """ A function for logging in user """
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print("User doesn't exists")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('discussion')
+        else:
+            print("username or password is incorrect")
+    return render(request, 'users/login_register.html')
+
 def profile(request, pk):
+    """ A function for retreving profile """
     profile = Profile.objects.get(id=pk)
     context ={'profile': profile}
     return render(request, 'users/profile.html', context)
