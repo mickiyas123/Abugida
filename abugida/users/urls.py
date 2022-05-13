8f644cb1e611e8faa2106eddc30424844534d7f1
mickiy123 <mikikinfe345@gmail.com>
2022-05-13 12:00:25 +0300

    Update functionality

--- abugida/users/urls.py
+++ abugida/users/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.hello, name="name")
+    path('profile/<str:pk>', views.profile, name="profile")
 ]
--- abugida/users/views.py
+++ abugida/users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 
 def profile(request, pk):
-    pass
+    profile = Profile.objects.get(id=pk)
+    context ={'profile': profile}
+    return render(request, 'users/profile.html', context)
